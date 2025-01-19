2fbd0dcdf24fe9b69b9b20bee8f6cb935979d871
Batajoo <sanjivbatajoo@gmail.com>
2025-01-18 23:20:15 -0600

    m add from a and b problem

--- array/revisionArrayConcepts/mAddfromAtoB.cpp
+++ array/revisionArrayConcepts/mAddfromAtoB.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-int find(int m, vector<pair<int, int>> q){
+int find(int m,vector<pair<int, int>> q){
     int mx = 0;
-
     vector<int> pre(5,0);
+    for(int i=0;i<m;i++){
+        int a = q[i].first;
+        int b = q[i].second;
 
-    for(int i=0; i<m; i++){
-        int a = q[i].first, b=q[i].second;
         pre[a-1] += 100;
         pre[b] -= 100;
     }
 
-    for(int i=1; i<5; i++){
+    for(int i=1;i<5;i++){
         pre[i] += pre[i-1];
-        mx=max(mx, pre[i]);
+        mx = max(mx, pre[i]);
     }
-    
+
     return mx;
 }
 
