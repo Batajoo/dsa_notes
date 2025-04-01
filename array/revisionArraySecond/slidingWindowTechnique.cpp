a5bd673e48a9edd433bfe6e3dc1fb2b5b2aeeac1
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-31 23:54:51 -0500

    sliding windom technique

---
+++ array/revisionArraySecond/slidingWindowTechnique.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+This technique is commonly used in algorithms like 
+    - finding subarrays with a specific sum, 
+    - finding the longest substring with unique characters, 
+    - or solving problems that require a fixed-size window to process elements efficiently.
+*/
+
+int main (){
+
+
+    return 0;
+}
