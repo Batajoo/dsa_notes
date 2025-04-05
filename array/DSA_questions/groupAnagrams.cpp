071a16de1a8dcd10018c957025c48d194b17b487
Sanjiv <sanjivbatajoo@gmail.com>
2025-04-04 23:53:48 -0500

    hash table implementation

--- array/DSA_questions/groupAnagrams.cpp
+++ array/DSA_questions/groupAnagrams.cpp
@@ -2,14 +2,15 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <unordered_map>
 using namespace std;
 
 vector<vector<string>> groupAnagrams(vector<string>& strs);
+vector<vector<string>> groupAnagramHashTable(vector<string> &strs);
 
 int main(){
-    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
-    vector<vector<string>> anagrams = groupAnagrams(strs);
+    vector<string> strs = {"act","pots","tops","cat","stop","hat"};
+    vector<vector<string>> anagrams = groupAnagramHashTable(strs);
     for(vector<string> anagramList: anagrams){
         for(string anagram: anagramList){
             cout << anagram << " ";
@@ -21,6 +22,42 @@ int main(){
 }
 
 vector<vector<string>> groupAnagrams(vector<string>& strs){
-    vector<string> sortedStrs = strs;
-    
+    unordered_map<string, vector<string>> res;
+
+    for(string str: strs){
+        string sortedWord = str;
+        sort(sortedWord.begin(), sortedWord.end());
+        res[sortedWord].push_back(str);
+    }
+
+    vector<vector<string>> result;
+    for(auto it = res.begin(); it != res.end(); it++){
+        result.push_back(it->second);
+    }
+    return result;
+}
+
+//hash table approach
+
+vector<vector<string>> groupAnagramHashTable(vector<string> &strs){
+    unordered_map<string, vector<string>> res;
+
+    for(const auto &s: strs){
+        vector<int> count(26,0);
+        for(char c: s){
+            count[c-'a']++;
+        }
+        string key = to_string(count[0]);
+        for(int i = 1;  i<26; i++){
+            key += ','+ to_string(count[i]);
+        }
+        cout << key << endl;
+        res[key].push_back(s);
+    }
+
+    vector<vector<string>> result;
+    for(const auto & pair: res){
+        result.push_back(pair.second);
+    }
+    return result;
 }
