f3dba866ac71abad7bf87b252695e128a34b2313
Sanjiv <sanjivbatajoo@gmail.com>
2025-04-07 23:54:46 -0500

    topKFrequentElements BucketSort method

--- array/DSA_questions/topKFrequentElements.cpp
+++ array/DSA_questions/topKFrequentElements.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 vector<int> topKFrequent(vector<int>& nums, int k);
+vector<int> topKFrequentBucketSort(vector<int>& nums, int k);
 
 int main(){
     vector<int> nums = {1,1,1,2,2,3};
     int k = 2;
-    vector<int> result = topKFrequent(nums,k);
+    vector<int> result = topKFrequentBucketSort(nums,k);
     for(auto res: result){
         cout << res << " ";
     }
@@ -29,6 +30,30 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
     vector<int> result;
     for(int i=0; i<k; i++){
         result.push_back(arr[i].second);
+    }
+
+    return result;
+}
+
+
+vector<int> topKFrequentBucketSort(vector<int>& nums, int k){
+    unordered_map<int, int> count;
+    vector<vector<int>> freq(nums.size() + 1);
+
+    for(int n : nums){
+        count[n]++;
+    }
+
+    for(const auto it: count){
+        freq[it.second].push_back(it.first);
+    }
+
+    vector<int> result;
+    for(int i = freq.size()-1; i > 0; i--){
+        for(int j = 0; j < freq[i].size(); j++){
+            result.push_back(freq[i][j]);
+            if(result.size() == k) return result;
+        }
     }
 
     return result;
