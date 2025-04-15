123c9f161ad8b8397d7e49e602dee299a997ec30
Sanjiv <sanjivbatajoo@gmail.com>
2025-04-14 23:51:58 -0500

    revision prefix sum

---
+++ array/revisionArraySecond/prefixSumRev.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findPrefixSum(vector<int> arr){
+    int n = arr.size();
+
+    vector<int> prefixSum(n);
+    
+    prefixSum[0] = arr[0];
+
+    for(int i = 1; i<n ;i++){
+        prefixSum[i] = prefixSum[i-1] + arr[i];
+    }
+
+    return prefixSum;
+}
+
+int main(){
+    vector<int> arr = {10, 20, 10, 5, 15};
+    vector<int> prefixSum = findPrefixSum(arr);
+    for(auto i:prefixSum) {
+        cout<<i<<" ";
+    }
+    return 0;
+}
