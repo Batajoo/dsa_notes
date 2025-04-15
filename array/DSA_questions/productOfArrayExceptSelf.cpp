04da3a5772d4570bec11973a2a7e1b22c9821ad5
Sanjiv <sanjivbatajoo@gmail.com>
2025-04-15 08:16:41 -0500

    product of array except self prefix method

--- array/DSA_questions/productOfArrayExceptSelf.cpp
+++ array/DSA_questions/productOfArrayExceptSelf.cpp
@@ -19,10 +19,34 @@ vector<int> productExceptSelf(vector<int>& nums) {
     return result;
 }
 
+vector<int> productExceptSelfPrefixMethod(vector<int> &nums){
+    int n = nums.size();
+
+    vector<int> res(n);
+    vector<int> prefix(n);
+    vector<int> suffix(n);
+
+    prefix[0] = 1;
+    for(int i=1; i<n; i++){
+        prefix[i] = prefix[i-1] * nums[i-1];
+    }
+
+    suffix[n-1] = 1;
+    for(int i=n-2; i>=0; i--){
+        suffix[i] = suffix[i+1] * nums[i+1];
+    }
+
+    for(int i=0; i<n; i++){
+        res[i] = prefix[i] * suffix[i];
+    }
+
+    return res;
+}
+
 int main(){
     vector<int> nums = {1,2,3,4};
-    vector<int> result = productExceptSelf(nums);
+    vector<int> result = productExceptSelfPrefixMethod(nums);
     for(int value: result){
-        cout << value << " - ";
+        cout << value << " ";
     }
 }
